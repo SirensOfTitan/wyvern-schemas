c4ddc29d22e1916fcbccb4900196f7d70bbfe4a0
Cole Potrocky <cole@vault.io>
2019-06-25 17:01:40 -0400

    Remove unused dependencies, pack final result properly.

--- dist/schemas/ERC20/index.js
+++ dist/schemas/ERC20/index.js
@@ -1,19 +1,16 @@
 "use strict";
-
-var _regenerator = require("babel-runtime/regenerator");
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
-
-var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var Web3 = require("web3");
-var types_1 = require("../../types");
-var unit = '1';
+const Web3 = require("web3");
+const types_1 = require("../../types");
+const unit = '1';
 exports.ERC20Schema = {
     version: 1,
     deploymentBlock: 0,
@@ -21,93 +18,73 @@ exports.ERC20Schema = {
     description: 'Items conforming to the ERC20 spec, using transferFrom.',
     thumbnail: 'https://opensea.io/static/images/opensea-icon.png',
     website: 'https://github.com/ethereum/eips/issues/20',
-    fields: [{ name: 'Address', type: 'address', description: 'Asset Contract Address' }],
-    assetFromFields: function assetFromFields(fields) {
+    fields: [
+        { name: 'Address', type: 'address', description: 'Asset Contract Address' },
+    ],
+    assetFromFields: (fields) => ({
+        address: fields.Address,
+    }),
+    assetToFields: asset => ({
+        Address: asset.address,
+    }),
+    formatter: (asset) => __awaiter(this, void 0, void 0, function* () {
         return {
-            address: fields.Address
+            title: 'ERC20 Asset at ' + asset.address,
+            description: '',
+            url: '',
+            thumbnail: '',
+            properties: [],
         };
-    },
-    assetToFields: function assetToFields(asset) {
-        return {
-            Address: asset.address
-        };
-    },
-    formatter: function () {
-        var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(asset) {
-            return _regenerator2.default.wrap(function _callee$(_context) {
-                while (1) {
-                    switch (_context.prev = _context.next) {
-                        case 0:
-                            return _context.abrupt("return", {
-                                title: 'ERC20 Asset at ' + asset.address,
-                                description: '',
-                                url: '',
-                                thumbnail: '',
-                                properties: []
-                            });
-
-                        case 1:
-                        case "end":
-                            return _context.stop();
-                    }
-                }
-            }, _callee, undefined);
-        }));
-
-        function formatter(_x) {
-            return _ref.apply(this, arguments);
-        }
-
-        return formatter;
-    }(),
+    }),
     functions: {
-        transfer: function transfer(asset) {
-            return {
-                type: Web3.AbiType.Function,
-                name: 'transferFrom',
-                payable: false,
-                constant: false,
-                stateMutability: types_1.StateMutability.Nonpayable,
-                target: asset.address,
-                inputs: [{ kind: types_1.FunctionInputKind.Owner, name: '_from', type: 'address' }, { kind: types_1.FunctionInputKind.Replaceable, name: '_to', type: 'address' }, { kind: types_1.FunctionInputKind.Asset, name: '_value', type: 'uint256', value: unit }],
-                outputs: []
-            };
-        },
-        transferQuantity: function transferQuantity(asset, quantity) {
-            return {
-                type: Web3.AbiType.Function,
-                name: 'transferFrom',
-                payable: false,
-                constant: false,
-                stateMutability: types_1.StateMutability.Nonpayable,
-                target: asset.address,
-                inputs: [{ kind: types_1.FunctionInputKind.Owner, name: '_from', type: 'address' }, { kind: types_1.FunctionInputKind.Replaceable, name: '_to', type: 'address' }, { kind: types_1.FunctionInputKind.Count, name: '_value', type: 'uint256', value: quantity }],
-                outputs: []
-            };
-        },
-        countOf: function countOf(asset) {
-            return {
-                type: Web3.AbiType.Function,
-                name: 'balanceOf',
-                payable: false,
-                constant: true,
-                stateMutability: types_1.StateMutability.View,
-                target: asset.address,
-                inputs: [{ kind: types_1.FunctionInputKind.Owner, name: '_owner', type: 'address' }],
-                outputs: [{ kind: types_1.FunctionOutputKind.Count, name: 'balance', type: 'uint' }],
-                assetFromOutputs: function assetFromOutputs(outputs) {
-                    return outputs.balance;
-                }
-            };
-        },
-        assetsOfOwnerByIndex: []
+        transfer: asset => ({
+            type: Web3.AbiType.Function,
+            name: 'transferFrom',
+            payable: false,
+            constant: false,
+            stateMutability: types_1.StateMutability.Nonpayable,
+            target: asset.address,
+            inputs: [
+                { kind: types_1.FunctionInputKind.Owner, name: '_from', type: 'address' },
+                { kind: types_1.FunctionInputKind.Replaceable, name: '_to', type: 'address' },
+                { kind: types_1.FunctionInputKind.Asset, name: '_value', type: 'uint256', value: unit },
+            ],
+            outputs: [],
+        }),
+        transferQuantity: (asset, quantity) => ({
+            type: Web3.AbiType.Function,
+            name: 'transferFrom',
+            payable: false,
+            constant: false,
+            stateMutability: types_1.StateMutability.Nonpayable,
+            target: asset.address,
+            inputs: [
+                { kind: types_1.FunctionInputKind.Owner, name: '_from', type: 'address' },
+                { kind: types_1.FunctionInputKind.Replaceable, name: '_to', type: 'address' },
+                { kind: types_1.FunctionInputKind.Count, name: '_value', type: 'uint256', value: quantity },
+            ],
+            outputs: [],
+        }),
+        countOf: asset => ({
+            type: Web3.AbiType.Function,
+            name: 'balanceOf',
+            payable: false,
+            constant: true,
+            stateMutability: types_1.StateMutability.View,
+            target: asset.address,
+            inputs: [
+                { kind: types_1.FunctionInputKind.Owner, name: '_owner', type: 'address' },
+            ],
+            outputs: [
+                { kind: types_1.FunctionOutputKind.Count, name: 'balance', type: 'uint' },
+            ],
+            assetFromOutputs: (outputs) => outputs.balance,
+        }),
+        assetsOfOwnerByIndex: [],
     },
     events: {
-        transfer: []
+        transfer: [],
     },
-    hash: function hash(asset) {
-        return asset.address;
-    }
+    hash: asset => asset.address,
 };
-//# sourceMappingURL=index.js.map
 //# sourceMappingURL=index.js.map
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,8 +5,6 @@ node scripts/gen_from_tokenbase.js
 echo 'Compiling Typescript...'
 rm -rf dist-tsc dist
 yarn run tsc
-echo 'Transpiling with Babel...'
-yarn run babel dist-tsc --out-dir dist --source-maps
 for DIR in $(ls src/schemas/main); do
   cp -r src/schemas/main/$DIR/*.json dist/schemas/main/$DIR
 done
