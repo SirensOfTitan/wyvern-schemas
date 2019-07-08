1d1a5433b89fcb8a2d02545d15e87a6349d85c57
Cole Potrocky <cole@vault.io>
2019-07-08 13:57:11 -0400

    pack and build package.

--- src/tokens/index.ts
+++
@@ -1,9 +0,0 @@
-import { NetworkTokens } from '../types';
-
-import { mainTokens } from './main/index';
-import { rinkebyTokens } from './rinkeby/index';
-
-export const tokens = {
-  rinkeby: rinkebyTokens,
-  main: mainTokens,
-};
